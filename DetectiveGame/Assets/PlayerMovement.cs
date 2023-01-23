029b3c253f37afc63fc4dde14f871a284b7571b6
PeterJKZhu <09jzhu@brightoncollege.net>
2023-01-23 15:59:36 +0000

    Animations finished

--- DetectiveGame/Assets/PlayerMovement.cs
+++ DetectiveGame/Assets/PlayerMovement.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 20;
+    public float speed = 4;
     public Vector2 motion;
     public Animator animator;
+    private float direction = 0f;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
         motion = new Vector2(Input.GetAxisRaw("Horizontal"),0);
         transform.Translate(motion * speed * Time.deltaTime);
 
+        direction = Input.GetAxisRaw("Horizontal");
+
+        if (direction > 0f)
+        {
+            transform.localScale = new Vector2(4f, 4f);
+        }
+        else if (direction < 0f)
+        {
+            transform.localScale = new Vector2(-4f, 4f);
+        }
+
         animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
         // takes negative float and makes it positive
-    }
-
-    void FixedUpate()
-    {
-
     }
 }
