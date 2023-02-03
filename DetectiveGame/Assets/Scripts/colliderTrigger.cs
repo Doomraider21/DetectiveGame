536d28fb9e0f9ee4e3bd6a0b4d62c5a21e0a004a
PeterJKZhu <09jzhu@brightoncollege.net>
2023-02-03 10:40:38 +0000

    enter-houses

--- DetectiveGame/Assets/Scripts/colliderTrigger.cs
+++ DetectiveGame/Assets/Scripts/colliderTrigger.cs
@@ -9,6 +9,7 @@ public class colliderTrigger : MonoBehaviour
     public Collider2D gameObject1;
     public Collider2D gameObject2;
     public int buildIndex;
+
     
 
     private void triggerInteract()
@@ -16,13 +17,48 @@ public class colliderTrigger : MonoBehaviour
         if ((gameObject1.IsTouching(gameObject2)) && (Input.GetKeyDown("e")))
         {
             SceneManager.LoadScene(sceneBuildIndex:buildIndex);
+
         }
     }
 
+<<<<<<< Updated upstream
 
+=======
 
     void Update()
     {
         triggerInteract();
     }
+
+}
+
+public class SceneLoadActions
+{
+    [SerializeField] private Transform Player; //drag player reference onto here
+    private Vector3 targetPosition; //here you store the position you want to teleport your player to
+
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneLoaded; //You add your method to the delegate
+    }
+>>>>>>> Stashed changes
+
+    private void OnDisable()
+    {
+<<<<<<< Updated upstream
+        triggerInteract();
+    }
+=======
+        SceneManager.sceneLoaded -= SceneLoaded;
+    }
+
+    //After adding this method to the delegate, this method will be called every time
+    //that a new scene is loaded. You can then compare the scene loaded to your desired
+    //scenes and do actions according to the scene loaded.
+    private void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Player.position = targetPosition;
+    }
+>>>>>>> Stashed changes
 }
